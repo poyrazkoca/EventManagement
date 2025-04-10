4d9dfeb6e50874e5a5cd286ce8e20658f37e6f0f
poyraz koca <poyraz@saatteknoloji.com.tr>
2025-04-11 02:03:35 +0300

    additions and updates

--- backend/db.php
+++ backend/db.php
@@ -5,7 +5,7 @@ $user = 'root';       // Default XAMPP user
 $pass = '';           // Default XAMPP has no password
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=3307;dbname=$db;charset=$charset";
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
--- backend/read_events.php
+++ backend/read_events.php
@@ -1,9 +1,11 @@
 <?php
 include 'db.php';
 
+// Fetch events
 $stmt = $conn->prepare("SELECT * FROM events ORDER BY event_date ASC");
 $stmt->execute();
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// Return the events as JSON
 echo json_encode($events);
 ?>
